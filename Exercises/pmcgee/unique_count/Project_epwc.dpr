ad687cbb86dd4c1a3e531de49d8901c3f894300e
Paul McGee <paulmcgee1969@gmail.com>
2025-04-22 08:11:57 +0800

    minmax in a rush

--- Exercises/pmcgee/unique_count/Project_epwc.dpr
+++ Exercises/pmcgee/unique_count/Project_epwc.dpr
@@ -2,10 +2,12 @@
 program Project_epwc;
 uses
   System.SysUtils,
-  U_unique_count in 'U_unique_count.pas';
+  U_unique_count in 'U_unique_count.pas',
+  U_MinMax_Element in '..\minmax_elem\U_MinMax_Element.pas';
 
 begin
    Unique_Count_Main;
+   MinMax_Main;
 
    readln;
 end.
